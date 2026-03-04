2cbb0005f1bb1b56c39fa60c7d8201b47e706785
VadimIgorevich <vadosrob@gmail.com>
2026-03-04 13:36:47 +0200

    kinit

--- src/components/orders/ExchangeForm.vue
+++ src/components/orders/ExchangeForm.vue
@@ -153,6 +153,28 @@ const handleSubmit = async () => {
 
   try {
     const res = await formStore.submitOrder();
+
+
+    const { action, fields } = res.payment
+
+    // создаём форму
+    const form = document.createElement('form')
+    form.method = 'POST'
+    form.action = action
+
+    Object.entries(fields).forEach(([key, value]) => {
+      const input = document.createElement('input')
+      input.type = 'hidden'
+      input.name = key
+      input.value = String(value)
+      form.appendChild(input)
+    })
+
+    document.body.appendChild(form)
+    form.submit()
+
+
+
     toast.success("Заявка успешно создана!");
     formStore.resetForm();
     router.push({ name: 'order-details', params: { id: res.data.id } });
